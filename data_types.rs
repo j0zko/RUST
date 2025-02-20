875749763eaf45f155f766214efb8b36552a0313
j0zko <jozefgunar952@gmail.com>
2025-02-20 18:05:30 +0100

    data types

---
+++ data_types.rs
@@ -0,0 +1,14 @@
+// declaring a variable
+
+fn main() {
+  let string = "StringSTRING"; //str
+  let ratio_float = 4.5;
+  let is_boolean = true;
+  let ic_char = "#";
+
+  println!("book name is : {}", string);
+  println!("company rating on 6 is: {} ",ratio_float);
+  println!("i dont know rust: {}",is_boolean);
+  println!("use this hastag: {}",ic_char);
+
+}

9badd61287de40ef73d501ecb7724d6369fab865
j0zko <jozefgunar952@gmail.com>
2025-02-27 19:03:43 +0100

    rust : learning , variables and datatypes

--- main.rs
+++ main.rs
@@ -1,5 +1,9 @@
+/*macro syntax is like functions isted of gen a fn
+macros are expanded into source code that gets conpiled with the rest of the code*/
+
+
 fn main(){
-   println!("rust");
-   let x = 5;
+   println!("rust");// macro syntax
+   
 }
 
---
+++ variables.rs
@@ -0,0 +1,15 @@
+/* variables : immutable - default : variables are immutable 
+variable cannot change
+variables :muttable (mut) :the value can be changed
+
+i32-bit signed integer type
+*/
+
+let mut bike = 32;
+let mut bike:integer =32;
+
+fn main(){
+  let mut bike:i32 = 20_000;
+  println!("fees is {}", bike);
+  
+}

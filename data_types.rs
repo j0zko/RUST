afb07ed5c16b7eefad157ae9fb944a5999b20c50
j0zko <jozefgunar952@gmail.com>
2025-02-27 19:12:10 +0100

    datatypes

--- data_types.rs
+++ data_types.rs
@@ -5,6 +5,13 @@ fn main() {
   let ratio_float = 4.5;
   let is_boolean = true;
   let ic_char = "#";
+// A String has three parts:
+    // 1.i A pointer to the heap memory
+    // 2. A length
+    // 3. A capacity
+    // These three values are stored on the stack, but the actual string data
+    // is on the heap
+    let s = String::from("hello");
 
   println!("book name is : {}", string);
   println!("company rating on 6 is: {} ",ratio_float);
